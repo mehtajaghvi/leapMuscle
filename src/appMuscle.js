daa1e8b76643afb428661b49811a5da8ae6f74b5
mehtajaghvi <jaghvi11@gmail.com>
2015-11-10 19:18:09 -0500

    adding all the code so far

---
+++ src/appMuscle.js
@@ -0,0 +1,48 @@
+var webSocket = require('ws'),
+    ws = new webSocket('ws://127.0.0.1:6437'),
+    five = require('johnny-five'),
+    leapMotion = require('./lib/leap'),	
+    board = new five.Board(),
+    ledThumb,ledIndex,ledMiddle,ledRing,ledPinky,frame;
+
+board.on('ready', function() {
+    ledThumb =  new five.Led(13); 
+    ledIndex =  new five.Led(12);
+    ledMiddle = new five.Led(10);
+    ledRing =   new five.Led(9); 
+    ledPinky =  new five.Led(8);  
+    this.pinMode(3, five.Pin.PWM);
+    this.pinMode(11, five.Pin.PWM);
+   // this.analogWrite(11, 255);			
+//////////////////////////////////////////////////////
+
+    ws.on('message', function(data, flags) {
+        frame = JSON.parse(data); 
+	//console.log(frame);
+        if (frame.hands && frame.hands.length > 0) {
+///////////////	
+	var pointable = frame.pointables[0];
+	var hand = frame.hands[0];
+	var fingers = frame.pointables.length;
+      
+       	if (fingers ==5){
+	    //this.analogWrite(11, 255);	
+	    ledThumb.on();
+	    ledIndex.on(); 
+	    ledMiddle.on(); 
+	    ledRing.on();
+	    ledPinky.on(); 
+}	else if (fingers == 0){
+	    ledThumb.off();
+	    ledIndex.off(); 
+	    ledMiddle.off(); 
+	    ledRing.off();
+	    ledPinky.off(); 
+}
+        }
+///////////////////////
+        else {
+            //console.log("False");
+        }
+    });
+});
